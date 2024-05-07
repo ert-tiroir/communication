be08755511a7b83807084cef1e31e4ddbbd8e097
Mr Thimote <mrthimote@gmail.com>
2024-05-07 11:35:37 +0200

    fixed esp_wifi

--- communication/esp_wifi.cpp
+++ communication/esp_wifi.cpp
@@ -15,19 +15,29 @@ struct buffer_t* txbuf;
 unsigned int amount = 0;
 
 int esp_wifi_client_connect () {
-    int status = WiFi.begin(ssid, password);
-    if (status != WL_CONNECTED) return 0;
+    if (WiFi.status() == WL_CONNECTED && client.connected())
+        return 1;
+    if (WiFi.status() != WL_CONNECTED) {
+        int status = WiFi.begin(ssid, password);
+        int retry  = 10;
+        while (status != WL_CONNECTED) {
+            delay(100);
+            if (retry == 0) return 0;
+            retry --;
+            continue ;
+        }
+    }
 
     if (client.connected()) return 1;
 
     client.connect(ip, port);
+    while (!client.connected()) delay(10);
 
     return 1;
 }
 int esp_wifi_server_connect () {
     WiFiClient newClient = server.available();
     if (!newClient) return client.connected();
-
     client = newClient;
     return 1;
 }
@@ -46,13 +56,17 @@ int esp_wifi_server_init (struct buffer_t *_rxbuf, const char* ssid, const char*
     IPAddress nm(255, 255, 255, 0);
 
     WiFi.softAPConfig(ip, ip, nm);
+    delay(1000);
 
     server.begin(port);
     return 1;
 }
-int esp_wifi_client_init (struct buffer_t *_rxbuf, const char* ssid, const char* password, IPAddress _ip, int _port) {
+int esp_wifi_client_init (struct buffer_t *_rxbuf, const char* _ssid, const char* _password, IPAddress _ip, int _port) {
     isClient = 1;
     ip = _ip; port = _port;
+
+    ssid = _ssid;
+    password = _password;
 
     rxbuf = _rxbuf;
 
@@ -68,21 +82,23 @@ int esp_wifi_load (struct buffer_t *buffer, unsigned int page_amount) {
 }
 
 int esp_wifi_recv () {
-    unsigned char* rxpage = readable_page(rxbuf);
-    if (!rxpage) return 0;
+    unsigned char* rxpage = writable_page(rxbuf);
+    if (rxpage == 0) return 0;
     if (client.available() < rxbuf->pag_size) return 0;
 
     client.read(rxpage, rxbuf->pag_size);
 
-    free_read(rxbuf);
+    free_write(rxbuf);
     return 1;
 }
 void esp_wifi_send (unsigned char* buffer, unsigned int size) {
     int amount = 0;
-    while (size >= 0) {
+    while (size > 0) {
         if (!esp_wifi_connect()) continue ;
 
-        size -= client.write(buffer, size);
+        int amount = client.write(buffer, size);
+        buffer += amount;
+        size   -= amount;
         while (esp_wifi_recv()) {} 
         
         amount ++;
@@ -98,12 +114,12 @@ int esp_wifi_tick () {
     esp_wifi_recv();
 
     if (amount != 0) {
-        unsigned char* write_page = writable_page(txbuf);
-        if (!write_page) return 1;
+        unsigned char* write_page = readable_page(txbuf);
+        if (write_page == 0) return 1;
     
         esp_wifi_send(write_page, txbuf->pag_size);
 
-        free_write(txbuf);
+        free_read(txbuf);
         amount --;
     }
 
